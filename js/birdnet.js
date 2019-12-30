a81a4ac9590503b30ed2d540be6155c0d497f1bd
Stefan Kahl <kahst@hrz.tu-chemnitz.de>
2019-12-30 13:56:13 +0100

    Initial app setup

---
+++ js/birdnet.js
@@ -0,0 +1,13 @@
+// Imports
+const tf = require('@tensorflow/tfjs');
+const load = require('audio-loader')
+const resampler = require('audio-resampler');
+const normalize = require('array-normalize')
+
+ /////////////////////////  DO AFTER LOAD ////////////////////////////
+ window.onload = function () {
+
+    
+
+};
+
