4eb3e50099307d91a1be1c57ba4818a55bb36b6b
Stefan Kahl <kahst@hrz.tu-chemnitz.de>
2019-12-30 18:06:46 +0100

    Show file dialog

--- js/birdnet.js
+++ js/birdnet.js
@@ -4,10 +4,39 @@ const load = require('audio-loader')
 const resampler = require('audio-resampler');
 const normalize = require('array-normalize')
 
+const CONFIG = {
+
+    sampleRate: 48000
+
+}
+
+var AUDIO_DATA = [];
+
  /////////////////////////  DO AFTER LOAD ////////////////////////////
  window.onload = function () {
 
     
 
 };
+
+function loadAudioFile(filePath) {
+
+    // load one file
+    load(filePath).then(function (buffer) {
+
+        // Resample
+        resampler(buffer, CONFIG.sampleRate, async function(event) {
+
+            // Get raw audio data
+            var AUDIO_DATA = event.getAudioBuffer().getChannelData(0);
+
+            // Normalize audio data
+            AUDIO_DATA = normalize(AUDIO_DATA)
+
+            console.log(AUDIO_DATA);
+
+        });
+
+    });
+}
 
