4ff8ffea769e4e332120e55bfd0af77f9b9e3983
Mattk70 <email@mattkirkland.co.uk>
2022-03-17 23:21:42 +0000

    Help files and menu items Updating functionality using Hazel

--- main.js
+++ main.js
@@ -1,7 +1,39 @@
-const {app, dialog, ipcMain, BrowserWindow} = require('electron');
+const {app, dialog, autoUpdater, ipcMain, BrowserWindow} = require('electron');
 const fs = require("fs");
+require('update-electron-app')();
 // In the main process:
 //require('@electron/remote/main').initialize()
+
+//Updater
+const server = 'https://chirpity-electron-releases.vercel.app';
+const url = `${server}/update/${process.platform}/${app.getVersion()}`
+
+autoUpdater.setFeedURL({ url })
+
+setInterval(() => {
+  autoUpdater.checkForUpdates()
+}, 60000)
+
+// Update handling
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+  }
+
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall()
+  })
+})
+
+autoUpdater.on('error', message => {
+  console.error('There was a problem updating the application')
+  console.error(message)
+})
+
 let mainWindow;
 let workerWindow;
 
