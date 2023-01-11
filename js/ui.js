59a3f3902e8e20c44bb030ca174375438b6f508d
Mattk70 <email@mattkirkland.co.uk>
2023-01-11 12:42:11 +0000

    Modals now draggable
    Downgraded systeminformation

--- js/ui.js
+++ js/ui.js
@@ -2929,6 +2929,23 @@ bodyElement.on('dragstart', e => {
 })
 
 
+$(".modal-header").on("mousedown", function(mousedownEvt) {
+    var $draggable = $(this);
+    var x = mousedownEvt.pageX - $draggable.offset().left,
+        y = mousedownEvt.pageY - $draggable.offset().top;
+    $("body").on("mousemove.draggable", function(mousemoveEvt) {
+        $draggable.closest(".modal-content").offset({
+            "left": mousemoveEvt.pageX - x,
+            "top": mousemoveEvt.pageY - y
+        });
+    });
+    $("body").one("mouseup", function() {
+        $("body").off("mousemove.draggable");
+    });
+    $draggable.closest(".modal").one("bs.modal.hide", function() {
+        $("body").off("mousemove.draggable");
+    });
+});
 ////////// Date Picker ///////////////
 
 $(function () {
