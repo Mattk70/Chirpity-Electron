41d19cd59a8b317699796ddfdec4cffe9701c2ec
Mattk70 <email@mattkirkland.co.uk>
2023-05-18 14:29:47 +0100

    Big Update: Eliminated UpdateRecord from worker, now use InsertManualRecord for both insert and update. New way of counting total that accounts for topRankin Unified summary SQL into one query Created a State Class, moved code to module Added callCount to records Most functions now rely on State values instead of relying on parameters New helper function to manage timelinePreferences. Now toggles time/position display
    Editing, deletion and XC comparisons now all in context menu
    
    Fixed bug in updateFileStart
    Fixed Audacity bug (again)
    Corrected entries in migrants list

---
+++ js/state.js
@@ -0,0 +1,79 @@
+const sqlite3 = require('sqlite3');
+
+export class State {
+    constructor(db) {
+        this.db = db,
+            this.mode = 'analyse', // analyse, explore, chart
+            this.sortOrder = 'dateTime',
+            this.filesToAnalyse = [],
+            this.limit = 500,
+            this.saved = new Set(), // list of files requested that are in the disk database
+            this.globalOffset = 0, // Current start number for unfiltered results
+            // filteredOffset is the only propoerty that is updated directly
+            this.filteredOffset = {}, // Current species start number for filtered results
+            this.selection = false,
+            this.blocked = [-1],
+            this.audio = { format: 'mp3', bitrate: 128, padding: false, fade: false, downmix: false, quality: 5, },
+            this.filters = { highPassFrequency: 0, lowShelfFrequency: 0, lowShelfAttenuation: -6, SNR: 0 },
+            this.detect = { nocmig: false, contextAware: false, confidence: 450 },
+            this.chart = { range: { start: undefined, end: undefined }, species: undefined },
+            this.explore = { range: { start: undefined, end: undefined } },
+            this.model = null,
+            this.predictionCount = 0
+    }
+
+
+    update(updates) {
+        function updateState(currentState, updatedValues) {
+            for (let key in updatedValues) {
+                if (Object.hasOwn(currentState, key)) {
+                    if (Array.isArray(updatedValues[key]) ||
+                        updatedValues[key] instanceof sqlite3.Database) {
+                        currentState[key] = updatedValues[key];
+                    }
+                    else if (
+                        typeof currentState[key] === 'object' &&
+                        typeof updatedValues[key] === 'object' 
+                    ) {
+                        // We don't want to look into the database objects
+                        updateState(currentState[key], updatedValues[key]);
+                    } else {
+                        if (key === 'confidence')  updatedValues[key]*= 10;
+                        currentState[key] = updatedValues[key];
+                    }
+                } else if (!['action', 'path', 'temp', 'lat', 'lon'].includes(key)) {
+                    //if (key.startsWith('_')) break
+                    console.warn(`Attempted to update state with invalid property: ${key}, ${updatedValues[key]} `);
+                }
+            }
+        }
+        updateState(this, updates);
+    }
+
+    changeMode({ mode, disk, memory }) {
+        this.mode = mode;
+        // Modes: analyse, chart, explore, selection, saved-analysis
+        if (['analyse', 'selection'].includes(mode)) {
+            this.db = memory;
+        } else {
+            this.db = disk;
+        }
+        // Reset pagination offsets
+        this.globalOffset = 0;
+        this.filteredOffset = {};
+    }
+
+
+    setFiles(files) {
+        console.log("Setting STATE, filesToAnalyse " + files);
+        this.update({ filesToAnalyse: files });
+    }
+
+    increment() {
+        if (++this.predictionCount === 5) {
+            this.predictionCount = 0
+        }
+        return this.predictionCount;
+    }
+
+}
