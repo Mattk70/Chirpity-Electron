8649ad225bf2a75ddce32e05d2d08bbde4ebdfaa
mattk70 <email@mattkirkland.co.uk>
2024-04-24 18:56:15 +0100

    added mac/win options for source dir in prebuild script

--- prebuild.js
+++ prebuild.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const path = require('path');
 
 // Define the directory where your source files are located
-const SOURCE_DIR = "C:/Users/simpo/PycharmProjects/Chirpity-Electron";
-
+let SOURCE_DIR = "C:/Users/simpo/PycharmProjects/Chirpity-Electron";
+if (!fs.existsSync(SOURCE_DIR)) SOURCE_DIR = "/Users/matthew/PycharmProjects/Chirpity-Electron"
 // Function to recursively search for patterns in files
 function searchPatterns(directory, patterns) {
     const files = fs.readdirSync(directory);
