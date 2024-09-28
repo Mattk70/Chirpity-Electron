a4739e54c94830ab1d2bbfefa391cbe58482376c
mattk70 <email@mattkirkland.co.uk>
2024-09-28 18:52:00 +0100

    Cut'n'paste fix for Mac

--- main.js
+++ main.js
@@ -30,21 +30,38 @@ console.warn = log.warn;
 console.error = log.error;
 autoUpdater.logger = log;
 autoUpdater.logger.transports.file.level = 'info';
+// Define the menu template
+const isMac = process.platform === 'darwin'; // macOS check
+const template = [
+    ...(isMac ? [{
+      label: 'Chirpity',
+      submenu: [
+        { role: 'about' },
+        { type: 'separator' },
+        { role: 'services' },
+        { type: 'separator' },
+        { role: 'hide' },
+        { role: 'hideOthers' },
+        { role: 'unhide' },
+        { type: 'separator' },
+        { role: 'quit' }
+      ]
+    }] : []),
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { role: 'selectAll' }
+      ]
+    }
+  ];
 
-const menu = Menu.buildFromTemplate([{
-    label: app.name,
-    submenu: [
-      { role: 'about' },
-      { type: 'separator' },
-      { role: 'services' },
-      { type: 'separator' },
-      { role: 'hide' },
-      { role: 'hideOthers' },
-      { role: 'unhide' },
-      { type: 'separator' },
-      { role: 'quit' }
-    ]
-  }]);
+const menu = Menu.buildFromTemplate(template);
 
 Menu.setApplicationMenu(menu);
 // Updates
