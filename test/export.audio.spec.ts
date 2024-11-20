5ab4c6fecc8ba60cd5b3d7dbacef5d2830a31ce8
mattk70 <email@mattkirkland.co.uk>
2024-11-20 19:49:12 +0000

    increased save timeout

--- test/export.audio.spec.ts
+++ test/export.audio.spec.ts
@@ -125,7 +125,7 @@ test('Can create/edit a manual record', async () => {
   // Call count updated
   expect(callCount).toBe('3');
   await page.keyboard.press('ControlOrMeta+s');
-  await page.waitForTimeout(1000);
+  await page.waitForTimeout(5000);
   const filename = await page.locator('#filename span.filename');
   // File name is blue - saved
   const hasClass = await filename.evaluate(el => el.classList.contains('text-info'));
