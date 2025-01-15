cf0035e23f6e31f9d8853791b54cad7ad4d0701c
Mattk70 <email@mattkirkland.co.uk>
2025-01-15 14:21:36 +0000

    Refactor test setup by removing redundant functions and utilizing runExampleAnalysis for improved clarity and maintainability

--- test/record.creation.spec.ts
+++ test/record.creation.spec.ts
@@ -12,6 +12,7 @@ import {
   stubMultipleDialogs
 } from 'electron-playwright-helpers';
 import { ElectronApplication, Page } from 'playwright';
+import {runExampleAnalysis} from './helpers'
 //import {Jimp} from 'jimp';
 
 let electronApp: ElectronApplication;
@@ -25,38 +26,6 @@ const appInfo = parseElectronApp(latestBuild)
 // set the CI environment variable to true
 process.env.CI = 'e2e';
 
-
-async function openExampleFile(page: Page){
-  await page.locator('#navBarFile').click()
-  await page.locator('#open-file').click()
-  await page.locator('#spectrogramWrapper').waitFor({state: 'visible'})
-}
-
-async function changeSettings(page: Page, type: string, elementID: string, value: any, timeout: number){
-  elementID = '#' + elementID;
-  await  page.locator('#navbarSettings').click();
-  // for Birdnet's 34%$
-  await page.locator('#confidence').fill('30');
-  if (type === 'select'){
-    await page.selectOption(elementID, value);
-  } else if (type === 'switch'){
-    await page.locator(elementID).setChecked(value);
-  }  else {
-    await page.locator(elementID).fill(value);
-  }
-  await page.locator('#close-settings').click();
-  // Wait ?
-  if (timeout) await page.waitForTimeout(timeout);
-}
-
-async function runExampleAnalysis(page: Page, model: string){
-  await openExampleFile(page)
-  await changeSettings(page,'select', 'model-to-use', model, 1000)
-
-  await  page.locator('#navbarAnalysis').click()
-  await page.locator('#analyse').click()
-  await  page.locator('#resultTableContainer').waitFor({state: 'visible'})
-}
 
 test.beforeAll(async () => {
 
