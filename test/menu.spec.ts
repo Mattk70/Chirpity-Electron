f6a7ef9bd8f9c1d89a697eb619934d41f21b437d
mattk70 <email@mattkirkland.co.uk>
2026-06-08 10:48:34 +0100

    roll that back

--- test/menu.spec.ts
+++ test/menu.spec.ts
@@ -170,23 +170,7 @@ test(`BirdNET finds a Chaffinch @ 78%`, async () => {
 })
 
 test(`Nocmig analyse works and second result is 61%`, async () => {
-  // Override the stub for the example file
-  await stubMultipleDialogs(electronApp, [
-      {
-        method: 'showOpenDialog',
-        value: {
-          filePaths: [process.env.CHAFFINCH_MP3_PATH || ""],
-          canceled: false,
-        },
-      },
-      {
-        method: 'showSaveDialog',
-        value: {
-          filePath: '/path/to/file',
-          canceled: false,
-        },
-      },
-    ])
+  // Set a custom timeout for this specific test (in milliseconds)
 
   await runExampleAnalysis(page,'chirpity');
   const callID = page.locator('#speciesFilter').getByText('Redwing (call)');
