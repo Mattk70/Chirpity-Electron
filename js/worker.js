7086da193f499ae8a6b90b75d082145f160f951a
Mattk70 <email@mattkirkland.co.uk>
2026-09-21 11:36:30 +0100

    update NUM_WORKERS when reducing threads

--- js/worker.js
+++ js/worker.js
@@ -504,6 +504,7 @@ async function handleMessage(e) {
           const worker = predictWorkers.pop();
           worker.terminate();
         }
+        NUM_WORKERS += delta;
       }
       break;
     }
